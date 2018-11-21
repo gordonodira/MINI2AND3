aa93154080bebe9ba077c63c43552ce5c721a4f4
gordonodira <go37@njit.edu>
2018-11-21 13:54:24 -0500

    Created answer blade files, question blade files, Question controller Answer controller and added the profile view

--- database/seeds/AnswersTableSeeder.php
+++ database/seeds/AnswersTableSeeder.php
@@ -11,7 +11,7 @@ class AnswersTableSeeder extends Seeder
      */
     public function run()
     { $users = App\User::inRandomOrder();
-        for ($i = 1; $i <= 6; $i++) {
+        for ($i = 1; $i <= 8; $i++) {
             $users->each(function ($user) {
                 $question = App\Question::inRandomOrder()->first();
                 $answer = factory(\App\Answer::class)->make();
