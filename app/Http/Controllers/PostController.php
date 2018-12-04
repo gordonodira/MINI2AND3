ded9792c89a1d8b71eeb487842529234b7e9443f
Gordon Odira <sellemap@gmail.com>
2018-12-04 15:36:03 -0500

    Restructured Post Controller and Post Model

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,65 +5,15 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
-
+use App\Post;
 class PostController extends Controller
 {
-    public function view()
-    {
-        //get current logged in use
-        $user = Auth::user();
-        //load post
-        $post = Post::find(1);
 
-        if ($user->can('view', $post)) {
-            echo "User Allowed to Update Post:{$post->id}";
-        } else {
-        echo    'Not Authorized';
-        }
+    public function index()
+    {
+        $posts = Post::published()->paginate();
+        return view('posts.index', compact('posts'));
     }
 
-    public function create()
-    {
-        //get current logged in use
-        $user = Auth::user();
-        //load post
+};
 
-
-        if ($user->can('create', Post::class)) {
-            echo "User Allowed to Create Post";
-        } else {
-          echo  'Not Authorized';
-
-        }
-        exit;
-    }
-
-    public function update()
-    {
-        //get current logged in use
-        $user = Auth::user();
-
-        //load post
-        $post = Post::find(1);
-
-        if ($user->can('update', $post)) {
-            echo "User Allowed to Update Post:{$post->id}";
-        } else {
-            echo 'Not Authorized';
-        }
-    }
-
-    public function delete()
-    {
-        //get current logged in use
-        $user = Auth::user();
-        //load post
-        $post = Post::find(1);
-
-        if ($user->can('delete', $post)) {
-            echo "User Allowed to Update Post:{$post->id}";
-        } else {
-            echo 'Not Authorized';
-        }
-    }
-}
