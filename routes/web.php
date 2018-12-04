257864bbf827b06c5d00226a4b27351b0fd11d56
Gordon Odira <sellemap@gmail.com>
2018-12-04 15:27:22 -0500

    Updated Routes

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/user/{user_id}/profile', 'ProfileController@create')->name('profile.create');
@@ -31,10 +32,24 @@ Route::post('/questions/{question_id}/answers/', 'AnswerController@store')->name
 Route::patch('/questions/{question_id}/answer/{answer_id}', 'AnswerController@update')->name('answers.update');
 Route::delete('/questions/{question_id}/answer/{answer_id}', 'AnswerController@destroy')->name('answers.destroy');
 
-Route::get('service/Post/view','PostController@view');
-Route::get('service/Post/create','PostController@create');
-Route::get('service/Post/update','PostController@update');
-Route::get('service/Post/delete','PostController@delete');
+
+
+
+
+
+Route::get('/', 'PostController@index');
+Route::get('/posts', 'PostController@index')->name('list_posts');
+Route::group(['prefix' => 'posts'], function () {
+    Route::get('/drafts', 'PostController@drafts')->name('list_drafts')->middleware('auth');
+    Route::get('/show/{id}', 'PostController@show')->name('show_post');
+    Route::get('/create', 'PostController@create')->name('create_post')->middleware('can:create-post');
+    Route::post('/create', 'PostController@store')->name('store_post')->middleware('can:create-post');
+    Route::get('/edit/{post}', 'PostController@edit')->name('edit_post')->middleware('can:update-post,post');
+    Route::post('/edit/{post}', 'PostController@update')->name('update_post')->middleware('can:update-post,post');
+    // using get to simplify
+    Route::get('/publish/{post}', 'PostController@publish')->name('publish_post')->middleware('can:publish-post');
+
+});
 
 
 Route::resources([
